90c5537c079402d09dcdb1f6d2953496c72f799d
HarshalAhirrao <harshalahirrao001@gmail.com>
2025-08-13 11:22:21 +0530

    Add sitemap generation with next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://olympus-gym.vercel.app/", // <-- change this to your site URL
+    generateRobotsTxt: true, // also creates robots.txt
+    sitemapSize: 5000, // optional
+};
