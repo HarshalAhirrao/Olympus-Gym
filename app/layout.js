39cf190dd82bfda8228128b69035bf5e7bcd06e1
HarshalAhirrao <harshalahirrao001@gmail.com>
2025-08-22 11:21:17 +0530

    added speed insight

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import LenisProvider from "./components/LenisProvider";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 
